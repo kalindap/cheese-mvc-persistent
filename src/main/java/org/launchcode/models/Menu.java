f13e93ee5b84f5b5332cd09a210db80446afde86
Kalinda Piper <kalindap@gmail.com>
2017-06-25 12:34:12 -0500

    Added categories that are displayed with cheeses; added menus that can display groups of cheeses

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,59 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kalindapiper on 6/22/17.
+ */
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public Menu() {}
+
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+}
